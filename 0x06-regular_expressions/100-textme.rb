3ff99c0fa3e43324f06ccf9c57bdbba7aac39236
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-01-05 15:58:57 +0200

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Output: [SENDER],[RECEIVER],[FLAGS]
 
-puts ARGV[0].scan(/\[from:(.+?)\] \[to:(.+?)\] \[flags:(.+?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?[0-9]:-?[0-9]:-?[0-9]:-?[0-9]:-?[0-9])\]/).join(',')
