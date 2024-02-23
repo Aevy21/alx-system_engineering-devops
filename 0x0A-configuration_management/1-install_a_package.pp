7612e8de3f96bebd175b1190708980b09216754a
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-23 16:30:23 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,20 @@
-# Install Flask using pip3
+# Puppet manifest file: flask_install.pp
 
-# Description:
-# This Puppet manifest ensures that Flask version 2.1.0 is installed using pip3, the package manager for Python 3.
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
 
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3.8'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3.8'],
 }
