4e99f2674c13921b072d83d645e5b0a5b3f58306
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-23 15:50:01 +0200

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,6 @@
 # Use exec resource to kill processes named "killmenow" if they exist
 exec { 'killmenow':
-  command => 'pkill killmenow',
+  command => '/usr/bin/pkill -f killmenow',
+  onlyif  => '/usr/bin/pgrep -f killmenow',
+  path    => '/usr/bin',
 }
