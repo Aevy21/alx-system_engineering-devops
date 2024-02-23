29c03e093bb69ba41dbc26e1ff3794d2bb5f35b1
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-02-23 17:15:36 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
-# Puppet manifest file: flask_install.pp
+# Puppet manifest file  flask_install.pp
 
-# Install Python 3.8.10
-package { 'python3.8':
-  ensure => latest,
-}
-
-# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
