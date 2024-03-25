a3f2aac1a616a52fa08d472760aafc4f04f0a36e
Avumile <Smithavery435@gmail.com>
2024-03-25 14:46:33 -0700

    fixing errors

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -18,20 +18,20 @@ if __name__ == "__main__":
                      user.get("username")) for user in user_details_python]
 
     task_dict = {}
-    for user in user_id_name:
-        todos_url_user = f"{todos_url}?userId={user[0]}"
+    for user_id, username in user_id_name:
+        todos_url_user = f"{todos_url}?userId={user_id}"
         response_tasks = requests.get(todos_url_user)
         all_tasks = response_tasks.json()
 
-        tasks_list = {[{"username": user[1],
+        tasks_list = [{"username": username,
                        "task": task.get("title"),
                        "completed": task.get("completed")
-                       } for task in all_tasks]}
+                       } for task in all_tasks]
 
-        task_dict.update({user[0]: tasks_list})
+        task_dict[user_id] = tasks_list
 
-    # Write data to JSON file
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(task_dict, json_file, indent=4)
+        # Write data to JSON file
+        with open("todo_all_employees.json", "w") as json_file:
+            json.dump(task_dict, json_file, indent=4)
 
-    print("Data exported to todo_all_employees.json successfully.")
+            print("Data exported to todo_all_employees.json successfully.")
