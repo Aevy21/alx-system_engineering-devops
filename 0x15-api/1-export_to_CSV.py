dc49c63b09c31255bffcd8f061c7facc5f2dc7ae
Avumile <Smithavery435@gmail.com>
2024-03-25 10:56:08 -0700

    errors on csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,6 +7,7 @@
 import json
 import requests
 from sys import argv
+import csv
 
 if __name__ == "__main__":
     user_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     response_user = requests.get(user_url)
     user_details_python = response_user.json()
     employee_name = user_details_python.get("name")
+    user_id = argv[1]
 
     # Fetch all tasks for the user
     todos_url = f"{todos_url}?userId={argv[1]}"
@@ -24,14 +26,23 @@ if __name__ == "__main__":
 
     # Filter completed tasks
     tasks_done = 0
+    # List to store task data
+    task_data = []
     for task in all_tasks:
         # Check if task is completed
         if task.get("completed"):
             tasks_done += 1
+            task_data.append(
+                [user_id, employee_name, "Completed", task.get('title')])
+        else:
+            task_data.append(
+                [user_id, employee_name, "Incomplete", task.get('title')])
 
-    print(f"Employee {employee_name} is done with tasks("
-          f"{tasks_done}/{len(all_tasks)}):")
-
-    for task in all_tasks:
-        if task.get("completed"):
-            print(f"\t{task.get('title')}")
+    # Export data to CSV
+        csv_file_name=f"{user_id}.csv"
+        with open(csv_file_name, mode='w', newline='') as file:
+            writer=csv.writer(file)
+            writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            writer.writerows(task_data
+                    print(f"CSV file '{csv_file_name}' created successfully."))
