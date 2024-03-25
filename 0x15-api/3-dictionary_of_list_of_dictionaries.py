cc1d0a82c292670e4b7708bd96505c204f4846ca
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-25 23:33:07 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,42 +4,33 @@
 """
 import json
 import requests
-from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1]  # Assuming user_id is passed as a command-line argument
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     todos_url = "https://jsonplaceholder.typicode.com/todos"
-    
+    user_url = "https://jsonplaceholder.typicode.com/users"
+
     # Fetch user details
-    response_user = requests.get(user_url)
-    user_details_python = response_user.json()
-    employee_name = user_details_python.get("name")
+    response_users = requests.get(user_url)
+    user_details_python = response_users.json()
     
-    # Fetch all tasks for the user
-    todos_url = f"{todos_url}?userId={user_id}"
-    response_tasks = requests.get(todos_url)
-    all_tasks = response_tasks.json()
-    
-    # Initialize dictionary to store tasks for all employees
-    all_employees_tasks = {}
+    # Extract user IDs and names
+    user_id_name = [(str(user.get("id")), user.get("username")) for user in user_details_python]
 
-    # Filter completed tasks for the user
-    tasks_done = []
-    for task in all_tasks:
-        task_details = {
-            "username": employee_name,
-            "task": task.get("title"),
-            "completed": task.get("completed")
-        }
-        tasks_done.append(task_details)
+    task_dict = {}
+    for user in user_id_name:
+        todos_url_user = f"{todos_url}?userId={user[0]}"
+        response_tasks = requests.get(todos_url_user)
+        all_tasks = response_tasks.json()
 
-    # Add tasks to the dictionary for the corresponding user_id
-    all_employees_tasks[user_id] = tasks_done
+        tasks_list = [{"username": user[1],
+                       "task": task.get("title"),
+                       "completed": task.get("completed")
+                       } for task in all_tasks]
+
+        task_dict.update({user[0]: tasks_list})
 
     # Write data to JSON file
-    output_file = "todo_all_employees.json"
-    with open(output_file, "w") as json_file:
-        json.dump(all_employees_tasks, json_file, indent=4)
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(task_dict, json_file, indent=4)
 
-    print(f"Data exported to {output_file} successfully.")
+    print("Data exported to todo_all_employees.json successfully.")
