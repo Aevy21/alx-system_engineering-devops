c16428ee60d55ee80083e12e68d69d30ffef399c
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-04-09 18:05:33 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -23,5 +23,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
+        return response.json().get("data", {}).get("subscribers", 0)
     return 0
