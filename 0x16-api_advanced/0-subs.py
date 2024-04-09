90ce05eabda4cb0874b5d8585d3d599a08c16c35
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-04-09 16:55:22 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,20 +9,24 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers for  given subreddit.
+    Returns the number of subscribers for a given subreddit.
 
     subreddit: (string) - the subreddit to be queried
 
     Return: 0 if the given subreddit is invalid; else the number
     of subscribers is returned
     """
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "querying_reddit_subscribers"}
 
-    reddit_response = requests.get(url, headers=headers,
-                                   allow_redirects=False).json()
-
-    subscribers = reddit_response.get("data", {}).get("subscribers", 0)
-    return subscribers
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        reddit_response = response.json()
+        subscribers = reddit_response.get("data", {}).get("subscribers", 0)
+        return subscribers
+    else:
+        return 0
