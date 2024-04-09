530cd98a65f13404fed40816f16a16cf2689ccf1
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-04-09 20:58:31 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,34 @@
+#!/usr/bin/python3
+"""
+This module contains a function that prints the titles of the
+top 10 hottest posts for a specified subreddit
+"""
 
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints the tites of the top 10 hottest posts for a specified subreddit
+
+    subreddit: (string) - the subreddit to be queried
+    """
+
+    if subreddit is not None and type(subreddit) is str:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {"User-Agent": "api-practice (by /u/Aevy21)"}
+        params = {"limit": 10}
+
+        reddit_resp = requests.get(url, params=params, headers=headers,
+                                   allow_redirects=False).json()
+
+        reddit_response_data = reddit_resp.get("data", {})
+
+        if reddit_response_data:
+            for data_entry in reddit_response_data:
+                if data_entry == "children":
+                    for entry in reddit_response_data[data_entry]:
+                        print(entry.get("data", {}).get("title", None))
+                    return
+
+    print(None)
