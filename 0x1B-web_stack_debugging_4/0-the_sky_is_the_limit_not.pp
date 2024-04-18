94443672380fa448a160e6f4ee2dce76b818386f
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-04-18 21:35:37 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,12 @@
 # Puppet manifest to increase the maximum number of connections an Nginx server can handle
 
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => template('nginx/nginx_default.conf.erb'),
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  notify  => Service['nginx'],
+exec { 'ngix-conf':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/default/nginx'],
+# Restart Nginx to apply the configuration changes
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
